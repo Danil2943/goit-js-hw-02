a539c333e155030ca2bd784362bd3e2cef173fbe
Danil2943 <venomous2314@gmail.com>
2024-02-05 21:12:24 +0200

    correcting code

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,9 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  let totalSum = quantity * pricePerDroid;
-  if (totalSum <= customerCredits) {
-    return "Insufficient funds!";
+  let totalPrice = quantity * pricePerDroid;
+  if (totalPrice > customerCredits) {
+    return 'Insufficient funds!';
   } else {
-    return `You ordered ${quantity} droids worth ${totalSum} credits!`;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
   }
 }
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,13 @@
 function checkForSpam(message) {
-  const upMessage = message.toUpperCase();
-  const spamWords = "SPAM";
-  const saleWords = "SALE";
-  return upMessage.includes(spamWords) || upMessage.includes(saleWords)
-    ? true
-    : false;
+  const toUpperCaseMessage = message.toUpperCase();
+  return (
+    toUpperCaseMessage.includes('SPAM') || toUpperCaseMessage.includes('SALE')
+  );
 }
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
